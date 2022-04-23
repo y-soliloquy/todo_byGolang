425d1332ff59273e39fc4ab1b3df0f2b22455d98
y-soliloquy <engineer.soliloquy@gmail.com>
2022-04-23 19:58:46 +0900

    viewの一部をlayoutとして共通化した上で表示する

--- app/controllers/route_main.go
+++ app/controllers/route_main.go
@@ -1,15 +1,19 @@
 package controllers
 
 import (
-	"html/template"
-	"log"
 	"net/http"
 )
 
 func top(w http.ResponseWriter, r *http.Request) {
-	t, err := template.ParseFiles("app/views/templates/top.html")
-	if err != nil {
-		log.Fatalln(err)
-	}
-	t.Execute(w, nil)
+	// t, err := template.ParseFiles("app/views/templates/top.html")
+	// if err != nil {
+	// 	log.Fatalln(err)
+	// }
+	// t.Execute(w, nil)
+
+	// 共通化したlayoutを用いてviewを表示する（"layout"と"top"はこの時点でtemplateフォルダ内にあるhtmlファイルの差している）
+	// generateHTML(w, nil, "layout", "top")
+
+	// generateHTMLの第二引数にnilではなく任意のものを設置してviewに渡す場合
+	generateHTML(w, "Hello World!!!!!", "layout", "top")
 }
