809592616a0c5faabc772292ae5b9ec8244c7091
y-soliloquy <engineer.soliloquy@gmail.com>
2022-04-23 21:20:22 +0900

    ユーザー登録画面の機能を実装（これより前のコミットメッセージ内「ログイン」は「ユーザー登録」とする）

--- app/controllers/route_auth.go
+++ app/controllers/route_auth.go
@@ -1,7 +1,30 @@
 package controllers
 
-import "net/http"
+import (
+	"log"
+	"net/http"
+	"todo_bygolang/app/models"
+)
 
 func signUp(w http.ResponseWriter, r *http.Request) {
-	generateHTML(w, nil, "layout", "public_navbar", "signup")
+	if r.Method == "GET" {
+		generateHTML(w, nil, "layout", "public_navbar", "signup")
+	} else if r.Method == "POST" {
+		err := r.ParseForm()
+		if err != nil {
+			log.Fatalln(err)
+		}
+		user := models.User{
+			Name:     r.PostFormValue("name"),
+			Email:    r.PostFormValue("email"),
+			PassWord: r.PostFormValue("password"),
+		}
+
+		if err := user.CreateUser(); err != nil {
+			log.Fatalln(err)
+		}
+
+		http.Redirect(w, r, "/", 302)
+
+	}
 }
