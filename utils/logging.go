ccc473310af6ba8e2467c95edb9f6aaad95ba355
y-soliloquy <engineer.soliloquy@gmail.com>
2022-04-23 15:39:14 +0900

    ログが書き込まれるようにする

--- utils/logging.go
+++ utils/logging.go
@@ -7,7 +7,7 @@ import (
 )
 
 func LoggingSettings(logFile string) {
-	logfile, err := os.OpenFile(logFile, os.O_CREATE|os.O_CREATE|os.O_APPEND, 0666)
+	logfile, err := os.OpenFile(logFile, os.O_RDWR|os.O_CREATE|os.O_APPEND, 0666)
 	if err != nil {
 		log.Fatalln(err)
 	}
