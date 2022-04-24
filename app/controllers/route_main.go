61aad1a9a9c27599fe5f0f8af429d093725242f7
y-soliloquy <engineer.soliloquy@gmail.com>
2022-04-24 15:08:42 +0900

    Todo編集画面を表示する

--- app/controllers/route_main.go
+++ app/controllers/route_main.go
@@ -3,6 +3,7 @@ package controllers
 import (
 	"log"
 	"net/http"
+	"todo_bygolang/app/models"
 )
 
 func top(w http.ResponseWriter, r *http.Request) {
@@ -70,5 +71,22 @@ func todoSave(w http.ResponseWriter, r *http.Request) {
 		}
 
 		http.Redirect(w, r, "/todos", 302)
+	}
+}
+
+func todoEdit(w http.ResponseWriter, r *http.Request, id int) {
+	sess, err := session(w, r)
+	if err != nil {
+		http.Redirect(w, r, "/login", 302)
+	} else {
+		_, err := sess.GetUserBySession()
+		if err != nil {
+			log.Println(err)
+		}
+		t, err := models.GetTodo(id)
+		if err != nil {
+			log.Println(err)
+		}
+		generateHTML(w, t, "layout", "private_navbar", "todo_edit")
 	}
 }
--- app/controllers/server.go
+++ app/controllers/server.go
@@ -4,6 +4,8 @@ import (
 	"fmt"
 	"html/template"
 	"net/http"
+	"regexp"
+	"strconv"
 	"todo_bygolang/app/models"
 	"todo_bygolang/config"
 )
@@ -30,6 +32,26 @@ func session(w http.ResponseWriter, r *http.Request) (sess models.Session, err e
 	return sess, err
 }
 
+var validPath = regexp.MustCompile("^/todos/(edit|update)/([0-9]+$)")
+
+func parseURL(fn func(http.ResponseWriter, *http.Request, int)) http.HandlerFunc {
+	return func(w http.ResponseWriter, r *http.Request) {
+		q := validPath.FindStringSubmatch((r.URL.Path))
+		if q == nil {
+			http.NotFound(w, r)
+			return
+		}
+
+		qi, err := strconv.Atoi(q[2])
+		if err != nil {
+			http.NotFound(w, r)
+			return
+		}
+
+		fn(w, r, qi)
+	}
+}
+
 func StartMainServer() error {
 	files := http.FileServer(http.Dir(config.Config.Static))
 	http.Handle("/static/", http.StripPrefix("/static/", files))
@@ -42,5 +64,6 @@ func StartMainServer() error {
 	http.HandleFunc("/todos", index)
 	http.HandleFunc("/todos/new", todoNew)
 	http.HandleFunc("/todos/save", todoSave)
+	http.HandleFunc("/todos/edit/", parseURL(todoEdit))
 	return http.ListenAndServe(":"+config.Config.Port, nil)
 }
