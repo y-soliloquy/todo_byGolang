930d59b2e15a2bb79d009a364ad173815055682f
y-soliloquy <engineer.soliloquy@gmail.com>
2022-04-24 15:21:30 +0900

    Todoの更新反映と表示をする処理の実装

--- app/controllers/route_main.go
+++ app/controllers/route_main.go
@@ -90,3 +90,29 @@ func todoEdit(w http.ResponseWriter, r *http.Request, id int) {
 		generateHTML(w, t, "layout", "private_navbar", "todo_edit")
 	}
 }
+
+func todoUpdate(w http.ResponseWriter, r *http.Request, id int) {
+	sess, err := session(w, r)
+	if err != nil {
+		http.Redirect(w, r, "/login", 302)
+	} else {
+		err := r.ParseForm()
+		if err != nil {
+			log.Println(err)
+		}
+		user, err := sess.GetUserBySession()
+		if err != nil {
+			log.Println(err)
+		}
+		content := r.PostFormValue("content")
+		t := &models.Todo{
+			ID:      id,
+			Content: content,
+			UserID:  user.ID,
+		}
+		if err := t.UpdateTodo(); err != nil {
+			log.Println(err)
+		}
+		http.Redirect(w, r, "/todos", 302)
+	}
+}
--- app/controllers/server.go
+++ app/controllers/server.go
@@ -65,5 +65,6 @@ func StartMainServer() error {
 	http.HandleFunc("/todos/new", todoNew)
 	http.HandleFunc("/todos/save", todoSave)
 	http.HandleFunc("/todos/edit/", parseURL(todoEdit))
+	http.HandleFunc("/todos/update/", parseURL(todoUpdate))
 	return http.ListenAndServe(":"+config.Config.Port, nil)
 }
