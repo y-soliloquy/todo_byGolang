842fd853573b9bf5d9d022683b6d1fc7c20b9b66
y-soliloquy <engineer.soliloquy@gmail.com>
2022-04-24 14:47:55 +0900

    Todo作成処理の実装

--- app/controllers/route_main.go
+++ app/controllers/route_main.go
@@ -50,3 +50,25 @@ func todoNew(w http.ResponseWriter, r *http.Request) {
 		generateHTML(w, nil, "layout", "private_navbar", "todo_new")
 	}
 }
+
+func todoSave(w http.ResponseWriter, r *http.Request) {
+	sess, err := session(w, r)
+	if err != nil {
+		http.Redirect(w, r, "/login", 302)
+	} else {
+		err = r.ParseForm()
+		if err != nil {
+			log.Println(err)
+		}
+		user, err := sess.GetUserBySession()
+		if err != nil {
+			log.Println(err)
+		}
+		content := r.PostFormValue("content")
+		if err := user.CreateTodo(content); err != nil {
+			log.Println(err)
+		}
+
+		http.Redirect(w, r, "/todos", 302)
+	}
+}
--- app/controllers/server.go
+++ app/controllers/server.go
@@ -41,5 +41,6 @@ func StartMainServer() error {
 	http.HandleFunc("/logout", logout)
 	http.HandleFunc("/todos", index)
 	http.HandleFunc("/todos/new", todoNew)
+	http.HandleFunc("/todos/save", todoSave)
 	return http.ListenAndServe(":"+config.Config.Port, nil)
 }
